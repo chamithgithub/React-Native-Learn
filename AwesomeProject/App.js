d8bb6a4537e7e0d95a858a2d67c52d0974d046d7
Cha1552164 <dchamith4@gmail.com>
2023-06-08 19:40:15 +0530

    update c

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,30 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button} from 'react-native';
+import { StyleSheet, Text, View,Button, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummytext}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <View>
-        <Text style={styles.dummytext}>hello world !!</Text>
-        <Button title='Click me'/>
-      </View>
+    <View style={styles.appContainer}>
+       <View style={styles.inputContainer}> 
+        <TextInput style={styles.textInput} placeholder='Your Course Goal'/>
+        <Button title='Add Goal'/>
+        </View>
+         <View> 
+        <Text>List of Goals </Text>
+      
+        </View> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer:{
+    padding:50
   },
-  dummytext:{
-    margin:16,
-    borderWidth:3,
-    color:'#f2f'
+  inputContainer:{
+    flexDirection:"row",
+    justifyContent:"space-between"
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:'#cccccc',
+    width:'80%',
+    marginRight:8,
+    padding:8
   }
+
 });
 
