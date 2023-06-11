083374478465758d2bb2e28c03c9e8ff82dde3d5
chamithgithub <dchamith4@gmail.com>
2023-06-11 22:32:46 +0530

    update c

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,96 +1,66 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View,Button, TextInput} from 'react-native';
 
 export default function App() {
+ const[enteredGoalText,setEnteredGoalText]=useState('');
+ const[CourseGoals,setCourseGoals]=useState([]);
 
- // const goalInputHandeler = (enterdText) =>{
- // console.log(enterdText);
- // }
+ const goalInputHandeler = (enterdText) =>{
+ setEnteredGoalText(enterdText);
+ }
 
- // const addGoalHandeler = () => {
+ const addGoalHandeler = () => {
+  // console.log(enteredGoalText);
+  setCourseGoals((currentCourseGoals) =>[...CourseGoals,enteredGoalText]);
+ }
 
- // }
-
-  // return (
-  //   <View style={styles.appContainer}>
-  //      <View style={styles.inputContainer}> 
-  //           <TextInput style={styles.textInput} placeholder='Your Course Goal' onChange={goalInputHandeler}/>
-  //           <Button title='Add Goal' onPress={addGoalHandeler}/>
-  //       </View>
-  //       <View styles={styles.goalContainer}> 
-  //           <Text>List of Goals </Text>
-  //       </View> 
-  //   </View>
-  // );
-
-  return(
-
-    <View style={{padding :50,flexDirection:'row',justifyContent:'center' }}>
-    <View 
-      style={{
-        flex:1,
-      backgroundColor :'red',
-      width:100,
-      height :100,
-      justifyContent:'center',
-      alignItems:'center'
-      
-      }}>
-    <Text>1</Text>
+  return (
+    <View style={styles.appContainer}>
+       <View style={styles.inputContainer}> 
+            <TextInput style={styles.textInput} placeholder='Your Course Goal'onChangeText={goalInputHandeler}/>
+            <Button title='Add Goal' onPress={addGoalHandeler}/>
+        </View>
+        <View styles={styles.goalContainer}> 
+           {
+            CourseGoals.map((goal) => <Text style={styles.goalItem} key={goal}>{goal}</Text>)
+           }
+        </View> 
     </View>
-    <View 
-      style={{
-        flex:2,
-      backgroundColor :'blue',
-      width:100,
-      height :100,
-      justifyContent:'center',
-      alignItems:'center'
-      
-      }}>
-    <Text>2</Text>
-    </View>
-    <View 
-      style={{
-        flex:3,
-      backgroundColor :'green',
-      width:100,
-      height :100,
-      justifyContent:'center',
-      alignItems:'center'
-      
-      }}>
-    <Text>3</Text>
-    </View>
-
-    </View>
-  )
-
+  );
 
 }
 
 const styles = StyleSheet.create({
-  // appContainer:{
-  //   flex:1,
-  //   paddingTop:50,
-  //   paddingHorizontal:16
-  // },
-  // inputContainer:{
-  //   flex: 1,
-  //   flexDirection:"row",
-  //   justifyContent:"space-between",
-  //   alignItems:"center",
-  //   marginBottom:24,
-  //   borderBottomWidth:1,
-  //   borderColor:'#cccccc'
-  // },
-  // textInput:{
-  //   // borderWidth:1,
-  //   // borderColor:'#cccccc',
-  //   width:'70%',
-  //   marginRight:8,
-  //   padding:8
-  // },
-  // goalContainer:{
-  //   flex:4,
-  // }
+  appContainer:{
+    flex:1,
+    paddingTop:50,
+    paddingHorizontal:16
+  },
+  inputContainer:{
+    felx:1,
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center",
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderColor:'#cccccc'
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:'#cccccc',
+    width:'70%',
+    marginRight:8,
+    padding:8
+  },
+  goalContainer:{
+    flex :4,
+  },
+  goalItem:{
+    margin:4,
+    padding:8,
+    color:'#ffff',
+    borderRadius:10,
+    backgroundColor:'#ee5665',
+
+  }
 });
