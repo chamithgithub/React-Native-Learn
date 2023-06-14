959f73b304f25ede3e5c95473c6a47bf9f292dd1
chamithgithub <dchamith4@gmail.com>
2023-06-14 23:36:47 +0530

    meals app update c

--- meals-app/screens/CategoriesScreen.js
+++ meals-app/screens/CategoriesScreen.js
@@ -1,15 +1,19 @@
 import { FlatList } from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
+import {useNavigation} from "@react-navigation/native"
 
 import {CATEGORIES} from "../data/dummy-data";
 
 
 
-const CategoriesScreen =({navigation}) =>{
+const CategoriesScreen =() =>{
+
+    const navigation = useNavigation();
 
     const renderCategoryItems =(itemData) =>{
+
         const pressHandler =()=>{
-            navigation.navigate("MealsOverviewScreen");
+            navigation.navigate("MealsOverviewScreen", {categoryId:itemData.item.id});
         }
         return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler}/>
     }
--- meals-app/screens/MealsOverviewScreen.js
+++ meals-app/screens/MealsOverviewScreen.js
@@ -1,8 +1,15 @@
 import { View,Text,StyleSheet } from "react-native";
 
-const MealsOverviewScreen = () => {
+import { MEALS } from "../data/dummy-data";
+import Meal from "../models/meal";
+
+const MealsOverviewScreen = ({route}) => {
+
+    const { categoryId } =route.params;
+
+    MEALS.filter(Meal =>Meal.categoryIds.indexOf(categoryId) >=0);
 return <View style={style.container}>
-<Text>MealsOverviewScreen</Text>
+    <Text>MealsOverviewScreen {categoryId}</Text>
 </View>
 }
 
